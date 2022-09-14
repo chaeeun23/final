52d19558a87a738e316b76141b40aa239af66b13
User <kimchan2634@gmail.com>
2022-09-14 16:51:19 +0900

    (채은) commit test

---
+++ finalproject/src/test/java/com/gd/finalproject/FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gd.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
