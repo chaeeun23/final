832118a785c2a3e4a6a1494d0ff7f3553e506b9d
유정미 <110160354+YuJeongmi0920@users.noreply.github.com>
2022-09-16 20:56:24 +0900

    Update TestService.java

--- finalproject/src/main/java/com/gd/finalproject/TestService.java
+++ finalproject/src/main/java/com/gd/finalproject/TestService.java
@@ -11,15 +11,17 @@ public class TestService {
         System.out.println("로직1");
         System.out.println("로직1");
         System.out.println("로직1");
-        System.out.println("로직4");
-        System.out.println("로직4");
-        System.out.println("로직4");
-        System.out.println("로직4");
+        System.out.println("타인 수정 로직4");
+        System.out.println("타인 수정 로직4");
+        System.out.println("타인 수정 로직4");
+        System.out.println("타인 수정 로직4");
         System.out.println("로직3");
         System.out.println("로직3");
         System.out.println("로직3");
         System.out.println("로직3");
         System.out.println("로직3");
+         System.out.println("타인 수정 로직4");
+         System.out.println("타인 수정 로직4");
     }
 
     public void test2() {
@@ -27,6 +29,8 @@ public class TestService {
         System.out.println("로직0");
         System.out.println("로직0");
         System.out.println("로직0");
+        System.out.println("타인 수정 로직4");
+        System.out.println("타인 수정 로직4");
         System.out.println("로직2");
         System.out.println("로직2");
         System.out.println("로직2");
@@ -36,6 +40,11 @@ public class TestService {
         System.out.println("로직2");
         System.out.println("로직2");
         System.out.println("로직3");
+        
+         System.out.println("타인 수정 로직4");
+         System.out.println("타인 수정 로직4");
+         System.out.println("타인 수정 로직4");
+         System.out.println("타인 수정 로직4");
     }
 
 }
