692b3023708da7adb156902501889b10896c1d07
유정미 <dbwjdal53@naver.com>
2022-09-16 20:53:33 +0900

    깃 테스트용

---
+++ finalproject/src/main/java/com/gd/finalproject/TestService.java
@@ -0,0 +1,41 @@
+package com.gd.finalproject;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+
+    public void test() {
+        System.out.println("로직1");
+        System.out.println("로직1");
+        System.out.println("로직1");
+        System.out.println("로직1");
+        System.out.println("로직2");
+        System.out.println("로직2");
+        System.out.println("로직2");
+        System.out.println("로직2");
+        System.out.println("로직3");
+        System.out.println("로직3");
+        System.out.println("로직3");
+        System.out.println("로직3");
+        System.out.println("로직3");
+    }
+
+    public void test2() {
+        System.out.println("로직1");
+        System.out.println("로직1");
+        System.out.println("로직1");
+        System.out.println("로직1");
+        System.out.println("로직2");
+        System.out.println("로직2");
+        System.out.println("로직2");
+        System.out.println("로직2");
+        System.out.println("로직3");
+        System.out.println("로직3");
+        System.out.println("로직3");
+        System.out.println("로직3");
+        System.out.println("로직3");
+    }
+
+}
