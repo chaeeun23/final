1df4aff2c908c1182b90c652c4d6fabe338ae4fd
User <kimchan2634@gmail.com>
2022-09-21 15:38:13 +0900

    (채은) 상담예약현황리스트(consultReservationList) 구현완료

---
+++ finalproject/src/main/java/com/gd/finalproject/service/ConsultReservationService.java
@@ -0,0 +1,95 @@
+package com.gd.finalproject.service;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gd.finalproject.commons.TeamColor;
+import com.gd.finalproject.mapper.ConsultReservationMapper;
+import com.gd.finalproject.vo.ConsultReservation;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j 
+@Service
+@Transactional
+public class ConsultReservationService {
+	@Autowired ConsultReservationMapper consultReservationMapper;
+	
+	//consultReservationList 상담예약리스트(달력)
+	public Map<String, Object> getConsultReservationListByMonth(int year, int month){
+		log.debug(TeamColor.CE + "[ConsultReservationService.getConsultReservationList.param] year : " + year);
+		log.debug(TeamColor.CE + "[ConsultReservationService.getConsultReservationList.param] month : " + month);
+		
+		Calendar now = Calendar.getInstance(); // ex) 2022.07.01
+		
+		if(year == -1) { // null 값으로 기본값인 -1이 들어왔을 때, 현재 연도
+			year = now.get(Calendar.YEAR);
+		}
+		if(month == -1) { // null 값으로 기본값인 -1이 들어왔을 때, 현재 월
+			month = now.get(Calendar.MONTH)+1;
+		}
+		if(month == 0) { // 0이 넘어올 시, 작년(year-1) 12월
+			month = 12;
+			year = year - 1;
+		}
+		if(month == 13) { // 13이 넘어올 시, 올해(year+1) 1월
+			month = 1;
+			year = year + 1;
+		}
+		
+		log.debug(TeamColor.CE + "[ConsultReservationService.getConsultReservationList] year : " + year);
+		log.debug(TeamColor.CE + "[ConsultReservationService.getConsultReservationList] month : " + month);
+		
+		// firstDay : 오늘 날짜를 먼저 구하여 날짜만 1일로 변경
+		Calendar firstDay = Calendar.getInstance(); // ex) 2022.04.19
+		firstDay.set(Calendar.YEAR, year);
+		firstDay.set(Calendar.MONTH, month - 1); // 자바 달력 API는 1월을 0으로, 2월을 1로, ... 12월을 11로 설정되어있다.
+		firstDay.set(Calendar.DATE, 1); // ex) 2022.04.01
+		int dayOfWeek = firstDay.get(Calendar.DAY_OF_WEEK);
+		// dayOfWeek는 일 1, 월 2, ..., 토 7
+		// startBlank는 일 0, 월 1, ..., 토 6
+		
+		// 1) startBlank : 달력 시작 시 필요한 공백 개수 (td 공백 개수)
+		int startBlank = dayOfWeek - 1; // 일 0, 월 1, ..., 토 6 -> 1일의 요일을 이용하여 구한다
+		
+		// 2) endDay :  마지막 날짜
+		int endDay = firstDay.getActualMaximum(Calendar.DATE);
+		
+		// 3) endBlank / startBlank와 endDay를 합의 결과에 endBlank를 더해서 7의 배수가 되도록
+		int endBlank = 0;
+		if ((startBlank + endDay) % 7 != 0) {
+			// 7에서 startBlank + endDay의 7로 나눈 나머지 값을 빼면
+			endBlank = 7 - ((startBlank + endDay) % 7);
+		} 
+		
+		// 4) totalTd : 전체 td 개수
+		int totalTd = startBlank + endDay + endBlank;
+		
+		// 5) consultReservationList 테이블 리스트
+		List<ConsultReservation> consultReservationListByMonth = consultReservationMapper.selectConsultReservationListByMonth(year,month);
+		
+		log.debug(TeamColor.CE + "[ConsultReservationService.getConsultReservationList] startBlank : " + startBlank);
+		log.debug(TeamColor.CE + "[ConsultReservationService.getConsultReservationList] endDay : " + endDay);
+		log.debug(TeamColor.CE + "[ConsultReservationService.getConsultReservationList] endBlank : " + endBlank);
+		log.debug(TeamColor.CE + "[ConsultReservationService.getConsultReservationList] totalTd : " + totalTd);
+		log.debug(TeamColor.CE + "[ConsultReservationService.getConsultReservationList] consultReservationListByMonth : " + consultReservationListByMonth);
+		
+		Map<String,Object> returnMap = new HashMap<String, Object>();
+		
+		returnMap.put("consultReservationListByMonth", consultReservationListByMonth);
+		returnMap.put("startBlank", startBlank);
+		returnMap.put("endDay", endDay);
+		returnMap.put("endBlank", endBlank);
+		returnMap.put("totalTd", totalTd);
+		returnMap.put("year", year);
+		returnMap.put("month", month);
+				
+		return returnMap;
+	}
+}
---
+++ finalproject/src/main/webapp/WEB-INF/view/employee/consultReservationList.jsp
@@ -0,0 +1,91 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<!-- header(로고, 네비게이션바) -->
+	<div>
+		<c:import url="/WEB-INF/resource/inc/header.jsp"></c:import>
+		<br> <br>
+
+	</div>
+<div class="container">
+		<h1 style="text-align: center;">상담 스케줄</h1>
+		<br>
+		<h4 style="text-align: center;">
+
+			<a
+				href="${pageContext.request.contextPath}/consultReservationList?year=${ year }&month=${ month - 1 }"
+				class="btn btn-primary">이전 달</a> &nbsp; ${ year }년 ${ month }월
+			&nbsp; <a
+				href="${pageContext.request.contextPath}/consultReservationList?year=${ year }&month=${ month + 1 }"
+				class="btn btn-primary">다음 달</a>
+		</h4>
+		<br>
+
+		<div class="mt-2" style="height: auto; width: auto;">
+			<table id="zero_config"
+				class="table table-striped table-bordered no-wrap">
+				<thead>
+					<tr>
+						<th>일</th>
+						<th>월</th>
+						<th>화</th>
+						<th>수</th>
+						<th>목</th>
+						<th>금</th>
+						<th>토</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<c:forEach var="i" begin="1" end="${totalTd}" step="1">
+							<c:choose>
+								<c:when test="${(i - startBlank) > 0 && i <= endDay+startBlank}">
+									<td style="height: 110px;" width="10%">${i - startBlank}
+										<div>
+											<c:forEach var="l" items="${ consultReservationListByMonth }">
+												<c:if test="${(l.consultDateDay) ==  (i - startBlank)}">
+													<div>
+														<a
+															href="${pageContext.request.contextPath}/consultReservationOne?consultReservationNo=${l.consultReservationNo}" class="btn btn-dark">${l.userId}/${l.consultPhone}</a>
+													
+													</div>
+												</c:if>
+											</c:forEach>
+										</div>
+									</td>
+								</c:when>
+
+								<c:when test="${(i - startBlank) < 1 }">
+									<td></td>
+								</c:when>
+
+								<c:when test="${i > endDay}">
+									<td></td>
+								</c:when>
+							</c:choose>
+							<c:if test="${ i % 7 == 0}">
+					</tr>
+					<tr>
+						</c:if>
+						</c:forEach>
+					</tr>
+				</tbody>
+			</table>
+
+		</div>
+	</div>
+	<!-- footer -->
+	<div>
+		<br>
+		<c:import url="/WEB-INF/resource/inc/footer.jsp"></c:import>
+	</div>
+
+</body>
+</html>
