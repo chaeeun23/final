97dc63ee8680e353e68c559e1b381281211d44b9
User <kimchan2634@gmail.com>
2022-09-21 15:37:18 +0900

    (채은)consultReservation 수정

--- finalproject/src/main/java/com/gd/finalproject/vo/ConsultReservation.java
+++ finalproject/src/main/java/com/gd/finalproject/vo/ConsultReservation.java
@@ -5,9 +5,9 @@ import lombok.Data;
 @Data
 public class ConsultReservation {
 	private int consultReservationNo; //상담예약번호
-	private String employeeId; //직원아이디
 	private String userId; //회원아이디
 	private String consultPhone; //전화번호
 	private String consultDate; //상담할 날짜
 	private String createDate; //상담을 예약한 날짜
+	private int consultDateDay; //21
 }
