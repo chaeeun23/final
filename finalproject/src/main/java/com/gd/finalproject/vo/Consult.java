a91c11f8abd741fcb07d01c5c344fc5356e19fa7
User <kimchan2634@gmail.com>
2022-09-22 15:32:37 +0900

    (채은) Consult vo 수정

--- finalproject/src/main/java/com/gd/finalproject/vo/Consult.java
+++ finalproject/src/main/java/com/gd/finalproject/vo/Consult.java
@@ -5,9 +5,8 @@ import lombok.Data;
 @Data
 public class Consult {
 	private int consultNo; //상담번호
+	private int consultReservationNo; //상담예약번호
 	private String employeeId; //직원아이디
-	private String userId; //회원아이디
-	private String consultPhone; //전화번호
 	private String consultContent; //상담내용
 	private String createDate; 
 	
